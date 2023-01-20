2ff39936fea24ad54a5b9cc8b5c9e32ae46dee48
Bekogeko <gulestanbekir@gmail.com>
2023-01-21 00:57:30 +0300

    Third Dimension added
    + ModelViewProj Matrix added to the shader.
    + Depth Test enabled so we can properly see z axis.
    + 3D goes brrrrr

--- OpenGLCrashCourse/Main.cpp
+++ OpenGLCrashCourse/Main.cpp
@@ -2,6 +2,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 
 #include "shaderClass.h"
 #include "Texture.h"
@@ -19,20 +23,26 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_CORE_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+
 	// Vertices coordinates
 	GLfloat vertices[] =
 	{ //     COORDINATES     /        COLORS      /   TexCoord  //
-		-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,    0.0f, 0.0f, // Lower left corner
-		-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	 0.0f, 1.0f, // Upper left corner
-		 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	 1.0f, 1.0f, // Upper right corner
-		 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	 1.0f, 0.0f  // Lower right corner
+		-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+		-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+		 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+		 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+		 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
 	};
 
 	// Indices for vertices order
 	GLuint indices[] =
 	{
-		0, 2, 1, // Upper triangle
-		0, 3, 2 // Lower triangle
+		0, 1, 2,
+		0, 2, 3,
+		0, 1, 4,
+		1, 2, 4,
+		2, 3, 4,
+		3, 0, 4
 	};
 
 
@@ -84,27 +94,64 @@ int main() {
 
 	one_eye.texUnit(shaderProgram, "text0", 0);
 
+	float rotation=0;
+	double prevTime = glfwGetTime();
+
+	glEnable(GL_DEPTH_TEST);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		// clear bg with color
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		// clear color
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
 		// bind shader for all later extras
 		shaderProgram.Activate();
+
+		double currentTime = glfwGetTime();
+
+		if (currentTime - prevTime >= 1 / 60)
+		{
+			rotation += 0.5f;
+			prevTime = currentTime;
+		}
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj= glm::mat4(1.0f);
+
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f,0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 100.0f);
+
+		int modelLoc = glGetUniformLocation(shaderProgram.ID,"model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		
+		int viewLoc = glGetUniformLocation(shaderProgram.ID,"view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		
+
+		int projLoc = glGetUniformLocation(shaderProgram.ID,"proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
+
+		//assigns a value to the uniform; NOTE: must always be done after activating the shader code
 		glUniform1f(uniID, 0.5);
+		//binds the texture so that is appears in rendering
 		one_eye.Bind();
 		
 		vao1.Bind();
 
-		glDrawElements(GL_TRIANGLES,9, GL_UNSIGNED_INT,0 );
+		glDrawElements(GL_TRIANGLES,sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
 
 		// swap new frame buffer
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
+
+		// sets max fps to screen hertz
+		glfwSwapInterval(1);
 	}
 
 	vao1.Delete();
