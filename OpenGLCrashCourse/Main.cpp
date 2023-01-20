488ae0647e9385b52f7b70b6aa4698d0778ff153
Bekogeko <gulestanbekir@gmail.com>
2023-01-20 08:51:09 +0300

    Add Basic Texture Support
    It can nativly support textures now although this functionality should be classified.
    + Texture Coordinates added to vertex data along with the color.
    +it has texture as an default behavior.
    + stb util library
    
    WARNING: the texture image should be both in resources and src at the same time to properly work. Also the textures color format is also holds an critical role for the opengl

--- OpenGLCrashCourse/Main.cpp
+++ OpenGLCrashCourse/Main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
+
 #include "shaderClass.h"
 #include "VAO.h"
 #include "EBO.h"
@@ -16,21 +18,22 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_CORE_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// Vertices coordinates
 	GLfloat vertices[] =
-	{
-		-0.5f,     -0.5f * float(sqrt(3)) / 3,      0.0f,	 0.8f,  0.3f,   0.02f,		// Lower left corner
-		0.5f,      -0.5f * float(sqrt(3)) / 3,      0.0f,    0.8f,  0.3f,   0.02f, 	// Lower right corner
-		0.0f,       0.5f * float(sqrt(3)) * 2 / 3,  0.0f,    1.0f,  0.6f,   0.32f,		// Upper corner
-		-0.5f / 2,  0.5f * float(sqrt(3)) / 6,      0.0f,	 0.9f,  0.45f,  0.17f,		// Inner left
-		0.5f / 2,   0.5f * float(sqrt(3)) / 6,      0.0f,	 0.9f,  0.45f,  0.17f,		// Inner right
-		0.0f,      -0.5f * float(sqrt(3)) / 3,      0.0f,	 0.8f,  0.3f,   0.02f		// Inner down
+	{ //     COORDINATES     /        COLORS      /   TexCoord  //
+		-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,    0.0f, 0.0f, // Lower left corner
+		-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	 0.0f, 1.0f, // Upper left corner
+		 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	 1.0f, 1.0f, // Upper right corner
+		 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	 1.0f, 0.0f  // Lower right corner
 	};
+
+	// Indices for vertices order
 	GLuint indices[] =
 	{
-		0, 3, 5, // Lower left triangle
-		3, 2, 4, // Lower right triangle
-		5, 4, 1 // Upper triangle
+		0, 2, 1, // Upper triangle
+		0, 3, 2 // Lower triangle
 	};
+
 
 
 
@@ -62,8 +65,9 @@ int main() {
 	VBO vbo1(vertices, sizeof(vertices));
 	EBO ebo1(indices, sizeof(indices));
 
-	vao1.LinkAttrib(vbo1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	vao1.LinkAttrib(vbo1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	vao1.LinkAttrib(vbo1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	vao1.LinkAttrib(vbo1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	vao1.LinkAttrib(vbo1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 
 	vao1.Unbind();
 	vbo1.Unbind();
@@ -72,6 +76,32 @@ int main() {
 
 	// Gets ID of uniform called "scale"
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+
+	// Texture
+	int widthImg, heightImg, numColCh;
+	unsigned char* bytes = stbi_load("texture.png", &widthImg, &heightImg, &numColCh,0);
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthImg, heightImg, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+
+	stbi_image_free(bytes);
+	glBindTexture(GL_TEXTURE_2D,0);
+
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	shaderProgram.Activate();
+	glUniform1i(tex0Uni, 0);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -82,6 +112,7 @@ int main() {
 		
 		shaderProgram.Activate();
 		glUniform1f(uniID, 0.5);
+		glBindTexture(GL_TEXTURE_2D, texture);
 
 		vao1.Bind();
 
@@ -97,6 +128,8 @@ int main() {
 	vbo1.Delete();
 	ebo1.Delete();
 	shaderProgram.Delete(); 
+
+	glDeleteTextures(1, &texture);
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
